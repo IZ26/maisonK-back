8e81dff24d323df72aebfe6c012dc1a04236e29a
IlanZerdoun <ilanyona.z@gmail.com>
2018-07-12 16:58:30 +0200

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const session = require('express-session');
+
 
 
 const indexRouter = require('./routes/index');
