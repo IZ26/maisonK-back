80d33ba55d683b115dcc639b2d0d22b48d9b1718
IlanZerdoun <ilanyona.z@gmail.com>
2018-07-13 10:02:37 +0200

    route for front

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(path.resolve(__dirname + '/../public/index.html'))
 });
 
 module.exports = router;
